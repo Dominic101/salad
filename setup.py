33ca9c0ca1e10cbb21efc0d6a5fcb6d45688e42d
Dominic <drmaggio@mit.edu>
2025-08-02 12:21:49 -0400

    fixing package installation

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='salad',
     version='0.1',
-    packages=find_packages(include=['eval']),
+    packages=find_packages(include=['salad', 'salad.*']),
 )
 
